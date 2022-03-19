8f51924583d8b9d329f4569263f30af3a48fa330
vikalpvarshney1 <vikalpvarshney012@gmail.com>
2022-03-19 22:35:59 +0530

    first commit

---
+++ countervikalp/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+// import { Parent } from "./Components/Parent"
+// import {Task} from "./Components/Task"
+import { Counter } from './Components/Counter';
+
+function App() {
+    return ( <
+        >
+        <
+        Counter / >
+        <
+        />
+    );
+}
+
+export default App;
